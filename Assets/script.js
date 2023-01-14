c31289cd6091c0332cfc5f2eeb4bd802876fde60
Ash <acst52@gmail.com>
2023-01-14 18:46:14 -0500

    Fixes to Task List & Confetti Code

--- Assets/script.js
+++ Assets/script.js
@@ -66,3 +66,66 @@ generateRandomQuote();
 
 // ********** QUOTE API CODE END **********
 
+
+// ********** TASK LIST CODE START **********
+
+// Now let's dynamically create our task input fields and checkable-list & store locally:
+
+// Task list variables:
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskBtn = document.getElementById('taskBtn');
+const toDoField = document.getElementById('toDoField');
+
+function createTask(event) {
+    event.preventDefault();  // prevented page refresh to fix bug of task immediately disappearing
+    const task = taskInput.value;
+            console.log(task);
+    const label = document.createElement("label");
+        label.setAttribute("for", "checkbox-nested");
+    const labelInput = document.createElement("input");
+        labelInput.setAttribute("type", "checkbox");
+        labelInput.setAttribute("name", task);
+        labelInput.checked = false;
+        label.textContent = taskInput.value + " ";
+        label.append(labelInput);
+        toDoField.append(label);
+            console.log(label);
+    // want tasks to generate on separate lines / vertically, not horiz, every time the fcn runs i.e. every time a task is added
+    const lineBr = document.createElement("br");
+        toDoField.append(lineBr);
+    // left to do in this fcn: save tasks + checked-or-not to local storage - not working
+        // PRoblem may be that label is being added as key. 
+        // Need to create single task object that has text, key for is checked
+    localStorage.setItem("task", JSON.stringify(task));
+    console.log(task);  // every time you add a new task, its pushed to a task array (name + checked status) - update that and set item again
+    localStorage.setItem("isChecked", JSON.stringify(labelInput.checked));
+    
+
+// CONFETTI CODE:
+    const checkbox = labelInput;
+    const confettiContainer = document.getElementById("confetti-container");
+    const colors = ["red", "purple", "pink", "blue", "green", "yellow", "orange"];
+
+    checkbox.addEventListener("change", function(){
+        if (this.checked) {
+            for (let i = 0; i < 400; i++) {
+                const confetti = document.createElement("div");
+                confetti.classList.add("confetti");
+                confettiContainer.appendChild(confetti);
+
+                // make confetti a random color from the color array; see CSS for colors:
+                confetti.classList.add(colors[Math.floor(Math.random()*colors.length)]);
+                confettiContainer.appendChild(confetti);
+                
+                // animate confetti falling:
+                confetti.style.left = Math.random() * 100 + "vw";
+                confetti.style.animation = `fall ${Math.random() + 1}s ease-in-out ${Math.random() * 3}s forwards`;
+            }
+        }
+    })
+// END OF CONFETTI CODE
+};
+
+// Gotta add an event listener to the Create Task button:
+taskBtn.addEventListener("click", createTask);
