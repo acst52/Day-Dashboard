cb4f8826c85437a1c3a542051b0973b58749442d
Lixman <lixman_d@outlook.com>
2023-01-19 03:41:31 -0500

    Added the Jokes API and the Activities API with the same style of the rest of the page

---
+++ Assets/ActivityIdea.js
@@ -0,0 +1,33 @@
+// Define the variables
+let idea=document.getElementById("Idea")
+console.log(idea)
+
+function generateactivitiesIdea() {
+
+    fetch("http://www.boredapi.com/api/activity/",{
+cache: 'reload',
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+        idea.innerHTML= data.activity;
+    } )
+     
+  .catch (err => console.error(err));
+	
+
+}
+
+const activitesAPI =document.getElementById("activitybutton")
+    // Fetch the data 
+    // Display the data found 
+  
+  // We call the fonction
+  generateactivitiesIdea()
+
+    // create a event listner to refresh the activity 
+    activitesAPI.addEventListener("click",generateactivitiesIdea )
+
+
